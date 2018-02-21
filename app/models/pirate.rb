cb074d63399ad2daa05ac8c54661007ea90f2388
harenarium <22226614+harenarium@users.noreply.github.com>
2018-02-21 10:36:27 -0500

    Done.

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,14 @@
 class Pirate
+  attr_accessor :name, :weight, :height
+  # should I use PIRATES variable instead?
+  @@all = []
+  def initialize(params)
+    @name = params[:name]
+    @weight = params[:weight]
+    @height = params[:height]
+    @@all << self
+  end
+  def self.all
+    @@all
+  end
 end
--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,17 @@
 class Ship
+  attr_accessor :name, :type, :booty
+  # should I use SHIPS variable instead?
+  @@all = []
+  def initialize(params)
+    @name = params[:name]
+    @type = params[:type]
+    @booty = params[:booty]
+    @@all << self
+  end
+  def self.all
+    @@all
+  end
+  def self.clear
+    @@all.clear
+  end
 end
